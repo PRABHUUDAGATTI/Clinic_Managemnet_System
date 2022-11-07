3c2641b7406bd01bc9ffe3f26fbb195dcdaf792e
Prabhu.Udagatti <Prabhu.Udagatti@valtech.com>
2022-11-07 10:54:37 +0530

    Created

---
+++ Clinic_Mgnt_System/Clinic_Mgnt_System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clinic_Mgnt_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
